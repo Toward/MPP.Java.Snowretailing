6145fc9a243400dbabb4fa944e5ee5a2248d96d0
Toward <kirillsmolovskii@outlook.com>
2017-05-19 04:22:44 +0300

    Add userEditPage

--- snowretailing/angular/App/Controllers/UsersEditController.js
+++ snowretailing/angular/App/Controllers/UsersEditController.js
@@ -2,8 +2,39 @@
     angular.module("Snowretailing").controller("UsersEditController", UsersEditController);
 
     function UsersEditController($scope,queryService,constantService) {
-        queryService.asyncGet(constantService.USERS_URL).then(function (response) {
-            $scope.users = response;
-        });
+        getUsers = function () {
+            queryService.asyncGet(constantService.USERS_URL).then(function (response) {
+                $scope.users = response;
+            });
+        };
+
+        $scope.showModal = function (modalMode, user) {
+            $scope.modalMode = modalMode;
+            if(user != null){
+                $scope.user = user;
+                $scope.user.roleId = user.role.id;
+            }
+            $('#modal').modal('toggle');
+        }
+
+        $scope.delete = function (id) {
+            queryService.asyncDelete(constantService.USERS_URL.concat("/").concat(id)).then(function () {
+                getUsers();
+            });
+        };
+
+        $scope.create = function (user) {
+            queryService.asyncPost(constantService.USERS_URL, user).then(function () {
+                getUsers();
+            });
+        };
+
+        $scope.update = function (user) {
+            queryService.asyncPut(constantService.USERS_URL, user).then(function () {
+                getUsers();
+            });
+        };
+
+        getUsers();
     };
 })();
